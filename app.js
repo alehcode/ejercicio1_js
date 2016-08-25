925e0944c5d6862c304ad1e73eae5da3b6058d23
Alexandra Lopez <sccp.alopez@gmail.com>
2016-08-25 15:09:07 -0500

    agregando archivos

---
+++ app.js
@@ -0,0 +1,9 @@
+var num1=0;
+var num2=0;
+
+	num1=prompt('ingresa primer numero' , '');
+	num2=prompt('ingresa el segundo numero' , '');
+
+		var suma= (parseInt(num1)+ parseInt(num2));
+
+		window.alert(suma);
